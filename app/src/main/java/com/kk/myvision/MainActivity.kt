3d7fa0fec2d06462d18d3fce7b5c4d33e3b49be0
kunal kumar <kunal@kwikpay.it>
2019-08-29 11:45:18 +0530

    MyVision First Comitt

--- app/src/main/java/com/kk/myvision/MainActivity.kt
+++ app/src/main/java/com/kk/myvision/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.kk.myvision
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getPhoto()
+    }
+
+ override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+     if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+         val imageBitmap = data!!.extras!!.get("data") as Bitmap
+         imageView.setImageBitmap(imageBitmap)
+     }
+
     }
 
 
 
+fun getPhoto(){
+    bt_photo.setOnClickListener{
+        dispatchTakePictureIntent()
+    }
+}
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
 
 
-
+    }
 }
