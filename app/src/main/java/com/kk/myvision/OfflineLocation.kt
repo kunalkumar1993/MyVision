4be86b32367a17b8c4e424d53cd639f7e0971f76
kunal kumar <kunal@kwikpay.it>
2019-10-07 12:13:17 +0530

    MyVision ImageDescription 7/10/2019

--- app/src/main/java/com/kk/myvision/OfflineLocation.kt
+++ app/src/main/java/com/kk/myvision/OfflineLocation.kt
@@ -26,6 +26,11 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
 
 
 /**
@@ -33,10 +38,15 @@ import com.google.android.gms.maps.SupportMapFragment
  * Copyright (c) 2019 KwikPay Limited. All rights reserved.
  */
 public class OfflineLocation :AppCompatActivity() ,OnMapReadyCallback{
+//val mMap = null
 
+    override fun onMapReady(googleMap: GoogleMap?) {
+       // mMap = googleMap
 
-    override fun onMapReady(p0: GoogleMap?) {
-     
+        // Add a marker in Sydney and move the camera
+       /* val sydney = LatLng(-34, 151)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
     }
 
     var locationManager: LocationManager? = null
